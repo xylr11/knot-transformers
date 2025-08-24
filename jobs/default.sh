6ecc98ed97fffc1f5d13d6eab0cc09b3a45b749c
xylr11 <kylecmarkham@gmail.com>
2025-08-23 21:33:34 -0600

    Increased model size for overnight training

--- jobs/default.sh
+++ jobs/default.sh
@@ -2,18 +2,16 @@
 #SBATCH --job-name=default
 #SBATCH --output=logs/job_default.out
 #SBATCH --error=logs/job_default.err
-#SBATCH --time=01:00:00
+#SBATCH --time=08:00:00
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=16G
-#SBATCH --mail-user=kylecmarkham@gmail.com
+#SBATCH --mail-user=kylecm11@byu.edu
 #SBATCH --mail-type=BEGIN
 #SBATCH --mail-type=END
 #SBATCH --mail-type=FAIL
 
 module load python/3.12
-module load cuda/13.0
-module load cudnn/9.12.0
 
 source .venv/bin/activate
 
