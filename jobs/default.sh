bc0bf1f9dadf577b510b48e62030819b27378aee
xylr11 <kylecmarkham@gmail.com>
2025-08-23 21:36:00 -0600

    Updated naming for job script, increased default hidden dim

--- jobs/default.sh
+++ jobs/default.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#SBATCH --job-name=default
-#SBATCH --output=logs/job_default.out
-#SBATCH --error=logs/job_default.err
+#SBATCH --job-name=large_default
+#SBATCH --output=logs/job_large_default.out
+#SBATCH --error=logs/job_large_default.err
 #SBATCH --time=08:00:00
 #SBATCH --gpus=1
 #SBATCH --cpus-per-task=4
@@ -15,4 +15,4 @@ module load python/3.12
 
 source .venv/bin/activate
 
-python train.py --config configs/default.json --save states/best_model_default.pt
+python train.py --config configs/default.json --save states/best_model_large_default.pt
